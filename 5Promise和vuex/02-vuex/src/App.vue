b3333e5125ce9e5491d34017916cf18681ed2370
touchingwang <touchingwang@163.com>
2022-08-14 21:55:18 +0800

    假期第二天学习v,时间21:55

---
+++ 5Promise和vuex/02-vuex/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h2>{{ $store.state.counter }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+<style>
+@import "./assets/css/base.css";
+</style>
--- 5Promise和vuex/02-vuex/src/main.js
+++ 5Promise和vuex/02-vuex/src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App";
-import router from "./router/index";
+import store from "./store";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
+  store,
   router,
   render: h => h(App)
 });
