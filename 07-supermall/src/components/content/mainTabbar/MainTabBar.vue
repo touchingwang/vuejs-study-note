a4f0d0a14adca7bac176d2331f2d13066cb2e6a4
touchingwang <touchingwang@163.com>
2022-08-18 00:13:43 +0800

    8/18

---
+++ 07-supermall/src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="pink">
+        <img slot="item-icon" src="~assets/img/tabbar/icon01.png" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/icon01_active.png"
+          alt=""
+        />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="pink">
+        <img slot="item-icon" src="~assets/img/tabbar/icon02.png" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/icon02_active.png"
+          alt=""
+        />
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="pink">
+        <img slot="item-icon" src="~assets/img/tabbar/icon03.png" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/icon03_active.png"
+          alt=""
+        />
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="pink">
+        <img slot="item-icon" src="~assets/img/tabbar/icon04.png" alt="" />
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/icon04_active.png"
+          alt=""
+        />
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar";
+import TabBarItem from "components/common/tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 07-supermall/src/network/request.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export function request(config, success) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: "http://152.136.185.210:7878/api/hy66",
+    timeout: 5000,
+  });
+
+  // axios的拦截器
+  instance.interceptors.request.use(
+    (config) => {
+      // console.log(config);
+
+      // 1.比如config中的一些信息不符合服务器的要求
+      // 2.比如每次发送网络请求时,都希望在界面中显示一个请求的图标
+      // 3.某些网络请求(比如登录(token)),必须携带一些特殊的信息
+      return config;
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+
+  //axios的响应拦截(拦截响应)
+  instance.interceptors.response.use(
+    (res) => {
+      return res.data; //只返回data部分,只需要data部分
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+
+  // 发送真正的网络请求
+  return instance(config);
+}
