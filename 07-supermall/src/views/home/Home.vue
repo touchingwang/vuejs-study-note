5e206ded3c3c23c90828ae9609480f904174320c
touchingwang <touchingwang@163.com>
2022-08-18 23:06:06 +0800

    项目首页部分完成

--- 07-supermall/src/views/home/Home.vue
+++ 07-supermall/src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
     <feature-view />
     <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
@@ -119,7 +119,7 @@ import FeatureView from "./childComps/FeatureView";
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
@@ -134,7 +134,7 @@ export default {
     return {
       banners: [],
       recommends: [],
-      doods: {
+      goods: {
         pop: { page: 0, list: [] },
         news: { page: 0, list: [] },
         sell: { page: 0, list: [] },
@@ -143,12 +143,25 @@ export default {
   },
   created() {
     // 1.请求多个数据'
-    getHomeMultidata().then((res) => {
-      // this.result = res
-      console.log(res);
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        // this.result = res
+        // console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.page[type].page += 1;
+      });
+    },
   },
 };
 </script>
@@ -166,9 +179,10 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+  z-index: 9;
 }
 .tab-control {
   position: sticky;
-  top: 43px;
+  top: 44px;
 }
 </style>
